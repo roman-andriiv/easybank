37223ca48a277e63e789265c91cf23a2ecd21970
Roman Andriiv <roman.andriyiv.dev@gmail.com>
2026-09-14 20:27:52 +0200

    📦 build(accounts): init project setup

---
+++ accounts/src/test/java/com/andriiv/easybank/accounts/AccountsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.andriiv.easybank.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
